ec7e56c11f44c0fbf7871dee9441c7454b3f19ac
Mathew, Anish K <anish.k.mathew@accenture.com>
2019-08-18 17:24:00 -0500

    updating shop page collection preview and item

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./collection-preview.styles.scss";
 
-const CollectionPreview = () => (
-  <div className='collection-preview'>
-    <h1 className='title'>Title</h1>
-      <div className='preview'>Hi</div>
+import CollectionItem from "./../collection-item/collection-item.component";
+
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title}</h1>
+    <div className="preview">
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
+        ))}
+    </div>
   </div>
 );
 
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import SHOP_DATA from './shop.data'
-class ShopPage extends React.Component {
+import React from "react";
+import SHOP_DATA from "./shop.data";
+import CollectionPreview from "./../../components/collection-preview/collection-preview.component";
 
-  constructor(props){
+class ShopPage extends React.Component {
+  constructor(props) {
     super(props);
 
     this.state = {
       collections: SHOP_DATA
-    }
+    };
   }
 
   render() {
-    return <div>SHOP PAGE</div>
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
   }
 }
 
